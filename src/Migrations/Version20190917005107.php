1ceb745d9897dec9380f5510d30df5b7c528c1d7
Tapiwa Musasa <babutaps@gmail.com>
2019-09-17 10:51:41 +1000

    First migration source

---
+++ src/Migrations/Version20190917005107.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190917005107 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE epl_league (id INT AUTO_INCREMENT NOT NULL, position INT NOT NULL, games_played INT NOT NULL, won INT NOT NULL, draw INT NOT NULL, lost INT NOT NULL, goals_for INT NOT NULL, goals_against INT NOT NULL, points INT NOT NULL, crest_url VARCHAR(400) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE epl_league');
+    }
+}
