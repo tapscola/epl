ea978e15d58c1b268433fc2616b0ee1c989c089f
tapscola <babutaps@gmail.com>
2019-09-20 09:10:30 +1000

    DB query and the template updates

--- src/Controller/EplLogController.php
+++ src/Controller/EplLogController.php
@@ -9,18 +9,37 @@
 namespace App\Controller;
 
 
+use App\Entity\EplLeague;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
+
 
 class EplLogController extends AbstractController
 {
   /**
    * @Route("/epl/log")
    */
-  public function index()
+  public function showEpl(EntityManagerInterface $em)
   {
-    return $this->render('epl_league/index.html.twig');
+    $repository = $em->getRepository(EplLeague::class);
+    /** @var EplLeague $teams */
+    $teams = $repository->findAll();
+    //dump($teams[0]);die;
+    return $this->render('epl_league/index.html.twig', [
+      'name' => $teams[0]->getName(),
+      'pos' => $teams[0]->getPosition(),
+      'played' => $teams[0]->getGamesPlayed(),
+      'won' => $teams[0]->getWon(),
+      'lost' => $teams[0]->getLost(),
+      'goalsfor' => $teams[0]->getGoalsFor(),
+      'goalsagainst' => $teams[0]->getGoalsAgainst(),
+      'draw' => $teams[0]->getDraw(),
+      'points' => $teams[0]->getPoints(),
+      'cresturl' => $teams[0]->getCrestUrl()
+    ]);
   }
 
 }
